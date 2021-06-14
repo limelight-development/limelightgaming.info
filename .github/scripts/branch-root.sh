211d363dda34e67a53bf3f122e150ea3c8dd0b8b
Joshua Piper <32164094+JoshPiper@users.noreply.github.com>
2021-06-14 18:47:34 +0100

    fix: that's backwards.

--- .github/scripts/branch-root.sh
+++ .github/scripts/branch-root.sh
@@ -6,7 +6,7 @@ case $GITHUB_REF in
 		echo "::set-output name=deploy_exclude::branches"
 		;;
 	*)
-		deploy_root=$(echo "$GITHUB_REF" | sed -e 's/heads\/refs\///' -e 's/\//-/' -e 's/ ~\^:\/\\//g' -e 's/^/branches\//')
+		deploy_root=$(echo "$GITHUB_REF" | sed -e 's/refs\/heads\///' -e 's/\//-/' -e 's/ ~\^:\/\\//g' -e 's/^/branches\//')
 		echo "::set-output name=deploy_root::$deploy_root"
 		echo "::set-output name=deploy_exclude::"
 		;;
