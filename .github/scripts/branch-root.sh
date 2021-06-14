7a06ee7e90c1189b3e6e17fe96244bbb53eb76b6
Joshua Piper <32164094+JoshPiper@users.noreply.github.com>
2021-06-14 18:42:44 +0100

    feat: add an exclusion var.

--- .github/scripts/branch-root.sh
+++ .github/scripts/branch-root.sh
@@ -3,9 +3,11 @@
 case $GITHUB_REF in
 	"heads/refs/main" | "heads/refs/master" )
 		echo "::set-output name=deploy_root::/"
+		echo "::set-output name=deploy_exclude::branches"
 		;;
 	*)
 		deploy_root=$(echo "$GITHUB_REF" | sed -e 's/heads\/refs\///' -e 's/\//-/' -e 's/ ~\^:\/\\//g' -e 's/^/branches\//')
 		echo "::set-output name=deploy_root::$deploy_root"
+		echo "::set-output name=deploy_exclude::"
 		;;
 esac
