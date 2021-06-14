3e7b18845e8df004375ddef334754072c9f49375
Joshua Piper <32164094+JoshPiper@users.noreply.github.com>
2021-06-14 19:03:03 +0100

    feat: add a script for handling deleted branches, since they use a different format.

---
+++ .github/scripts/del-branch-root.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+case $deleted_ref in
+	"main" | "master" )
+		echo "::set-output name=deploy_root::/"
+		echo "::set-output name=deploy_exclude::branches"
+		;;
+	*)
+		deploy_root=$(echo "$GITHUB_REF" | -e 's/\//-/' -e 's/ ~\^:\/\\//g' -e 's/^/branches\//')
+		echo "::set-output name=deploy_root::$deploy_root"
+		echo "::set-output name=deploy_exclude::"
+		;;
+esac
