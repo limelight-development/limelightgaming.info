9379733fbb26e0e97d9d591559714e97c6083ae0
Joshua Piper <32164094+JoshPiper@users.noreply.github.com>
2021-06-14 19:44:48 +0100

    fix: wrong ref.

--- .github/scripts/del-branch-root.sh
+++ .github/scripts/del-branch-root.sh
@@ -6,7 +6,7 @@ case $deleted_ref in
 		echo "::set-output name=deploy_exclude::branches"
 		;;
 	*)
-		deploy_root=$(echo "$GITHUB_REF" | sed -e 's/\//-/' -e 's/ ~\^:\/\\//g' -e 's/^/branches\//')
+		deploy_root=$(echo "$deleted_ref" | sed -e 's/\//-/' -e 's/ ~\^:\/\\//g' -e 's/^/branches\//')
 		echo "::set-output name=deploy_root::$deploy_root"
 		echo "::set-output name=deploy_exclude::"
 		;;
