392442b429a02d0746f55be858e9a451447699b2
Joshua Piper <32164094+JoshPiper@users.noreply.github.com>
2021-06-17 12:34:15 +0100

    fix: properly handle baseurl for branch'd sites. (#21)

--- .github/scripts/branch-root.sh
+++ .github/scripts/branch-root.sh
@@ -4,10 +4,12 @@ case $GITHUB_REF in
 	"refs/heads/main" | "refs/heads/master" )
 		echo "::set-output name=deploy_root::/"
 		echo "::set-output name=deploy_exclude::branches"
+		echo "::set-output name=deploy_baseurl::"
 		;;
 	*)
 		deploy_root=$(echo "$GITHUB_REF" | sed -e 's/refs\/heads\///' -e 's/\//-/' -e 's/ ~\^:\/\\//g' -e 's/^/branches\//')
 		echo "::set-output name=deploy_root::$deploy_root"
 		echo "::set-output name=deploy_exclude::"
+		echo "::set-output name=deploy_baseurl::$deploy_root"
 		;;
 esac
