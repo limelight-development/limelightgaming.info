0dcc8624ce18310fe4f364188ffe3f324c094775
Joshua Piper <32164094+JoshPiper@users.noreply.github.com>
2021-06-17 12:18:37 +0100

    fix: use the actual deploy root

--- .github/scripts/branch-root.sh
+++ .github/scripts/branch-root.sh
@@ -10,6 +10,6 @@ case $GITHUB_REF in
 		deploy_root=$(echo "$GITHUB_REF" | sed -e 's/refs\/heads\///' -e 's/\//-/' -e 's/ ~\^:\/\\//g' -e 's/^/branches\//')
 		echo "::set-output name=deploy_root::$deploy_root"
 		echo "::set-output name=deploy_exclude::"
-		echo "::set-output name=deploy_baseurl::/branches/$deploy_root/"
+		echo "::set-output name=deploy_baseurl::$deploy_root"
 		;;
 esac
