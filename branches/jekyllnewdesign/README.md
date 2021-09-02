48d861c74a165d144467f216b63a7ce34ce710bd
Bambolicious <bambolimelight@gmail.com>
2021-09-02 20:03:55 +0000

    Deploying to gh-pages from @ limelight-development/limelightgaming.info@2e77984160dcb8f3b2d5d4120cf328a0a98e7980 🚀

---
+++ branches/jekyllnewdesign/README.md
@@ -0,0 +1,8 @@
+# limelightgaming.info
+The basic informational site for Limelight Development.
+
+## Deployment
+
+Deployment is handed through Actions, deplying to the gh-pages branch.
+Master branch is deployed to /, while all other branches are deployed to branches/<slug>.
+Local development & testing should take place using the provided gems.
---
+++ branches/jekyllnewdesign/assets/js/main.js
@@ -0,0 +1,54 @@
+//
+// Add a class to the header bar if we're scrolled down
+//
+$(window).scroll(function() {
+  var header = $(".nav.container");
+
+  var scroll = $(window).scrollTop();
+  if (scroll >= 80) {
+    header.addClass("nav-scroll");
+  } else {
+    header.removeClass("nav-scroll");
+  }
+});
+
+//
+// Swap server section background
+//
+$('.server-single').mouseenter(function () {
+  var serverBG = $(this).find('.server-banner').data('server-bg');
+  var serverImg = $('#server-bg').css('background-image');
+
+  var cleanup = /\"|\'|\)/g;
+  var serverBGsource = serverBG.split('/').pop().replace(cleanup, '');
+  serverImg = serverImg.split('/').pop().replace(cleanup, '');
+
+  if (serverImg != serverBGsource) {
+    $('#server-bg').css({'background-image': 'url('+serverBG+')', 'animation-name': ''});
+    $('#server-bg').width();
+    $('#server-bg').css('animation-name', 'bg-zoom');
+  }
+});
+
+//
+// Add class to nav as user scrolls through page
+//
+var addClassOnScroll = function () {
+  var windowTop = $(window).scrollTop();
+  $('.container[id]').each(function (index, elem) {
+    var offsetTop = $(elem).offset().top;
+    var outerHeight = $(this).outerHeight(true);
+
+    if( windowTop > (offsetTop - 350) && windowTop < ( offsetTop + outerHeight)) {
+      var elemId = $(elem).attr('id');
+      $(".nav-links a.active").removeClass('active');
+      $(".nav-links a[href='#" + elemId + "']").addClass('active');
+    }
+  });
+};
+
+$(function () {
+  $(window).on('scroll', function () {
+    addClassOnScroll();
+  });
+});
