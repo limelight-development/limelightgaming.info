24c1ff321edb506852efa48cca91d4476b5f776a
JoshPiper <32164094+JoshPiper@users.noreply.github.com>
2025-03-05 14:08:16 +0000

    build: Migrate branch deletion too.

--- .github/scripts/del-branch-root.sh
+++ .github/scripts/del-branch-root.sh
@@ -2,12 +2,12 @@
 
 case $deleted_ref in
 	"main" | "master" )
-		echo "::set-output name=deploy_root::/"
-		echo "::set-output name=deploy_exclude::branches"
+		echo "deploy_root=/" >> "$GITHUB_OUTPUT"
+		echo "deploy_exclude=branches" >> "$GITHUB_OUTPUT"
 		;;
 	*)
-		deploy_root=$(echo "$deleted_ref" | sed -e 's/\//-/' -e 's/ ~\^:\/\\//g' -e 's/^/branches\//')
-		echo "::set-output name=deploy_root::$deploy_root"
-		echo "::set-output name=deploy_exclude::"
+		deploy_root=$(echo "$GITHUB_REF" | iconv -t ascii//TRANSLIT | sed -e 's/refs\/heads\///' -e 's/\//-/g' -e 's/ ~\^:\/\\//g' -e 's/[^a-zA-Z0-9]+/-/g' -e 's/^-+\|-+$//g' -e 's/^/branches\//' | tr A-Z a-z)
+		echo "deploy_root=$deploy_root" >> "$GITHUB_OUTPUT"
+		echo "deploy_exclude=" >> "$GITHUB_OUTPUT"
 		;;
 esac
