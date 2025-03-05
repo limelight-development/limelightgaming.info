c2fa97163500b51f050987bb76e59e4fe5b4b8f7
JoshPiper <32164094+JoshPiper@users.noreply.github.com>
2025-03-05 14:48:09 +0000

    build: Get build scripts from main.

--- .github/scripts/del-branch-root.sh
+++ .github/scripts/del-branch-root.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
-case $GITHUB_REF in
-	"refs/heads/main" | "refs/heads/master" )
+case $deleted_ref in
+	"main" | "master" )
 		echo "deploy_root=/" >> "$GITHUB_OUTPUT"
 		echo "deploy_exclude=branches" >> "$GITHUB_OUTPUT"
-		echo "deploy_baseurl=" >> "$GITHUB_OUTPUT"
 		;;
 	*)
 		deploy_root=$(echo "$GITHUB_REF" | iconv -t ascii//TRANSLIT | sed -e 's/refs\/heads\///' -e 's/\//-/g' -e 's/ ~\^:\/\\//g' -e 's/[^a-zA-Z0-9]+/-/g' -e 's/^-+\|-+$//g' -e 's/^/branches\//' | tr A-Z a-z)
 		echo "deploy_root=$deploy_root" >> "$GITHUB_OUTPUT"
 		echo "deploy_exclude=" >> "$GITHUB_OUTPUT"
-		echo "deploy_baseurl=$deploy_root" >> "$GITHUB_OUTPUT"
 		;;
 esac
